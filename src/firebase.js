db0ed146948424317d9c4961cc198665915fd5c7
vinod selpol <vinodselpol@gmail.com>
2021-11-08 18:50:06 +0100

    Login

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCtE-7e6gyMsjhdpMh4yOfEykHi278qLo0",
@@ -8,3 +10,10 @@ const firebaseConfig = {
   appId: "1:126099630014:web:b6153aa7fb55339aaefbef",
   measurementId: "G-TBB2DCCT4G",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
